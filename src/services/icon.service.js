2d193640c6a08ff9a4bb3496ea727a9abf3477b5
Cleber Sanches <clebersanches59@gmail.com>
2024-03-04 21:10:26 -0300

    ♻️ refactor: Simplificação da função de geração de ícones

--- src/services/icon.service.js
+++ src/services/icon.service.js
@@ -1,38 +1,13 @@
-const { readdir, readFile } = require("fs/promises");
-const { join, resolve } = require("path");
 
-const CAMINHO_DIRETORIO_ICONES = resolve(__dirname, "../../icons");
 const NOME_ICONE_ERRO = "Error404";
 
-const carregarIcones = async () => {
-  const icones = {};
-  try {
-    const arquivosIcones = await readdir(CAMINHO_DIRETORIO_ICONES);
-
-    for (const arquivo of arquivosIcones) {
-      const nomeIcone = arquivo.replace(/\.svg$/, "");
-      const caminhoIcone = join(CAMINHO_DIRETORIO_ICONES, arquivo);
-
-      const dados = await readFile(caminhoIcone, "utf8");
-      icones[nomeIcone] = dados;
-    }
-  } catch (erro) {
-    console.error(`Erro ao carregar ícones: ${erro.message}`);
-    throw erro;
-  }
-
-  return icones;
-};
-
-const gerarIcones = (nomesIcones, iconesPorLinha, icones, tamanho) => {
+const gerarIcones = (nomesIcones, iconsPerLine, icones, tamanho) => {
   const ESCALA = tamanho / (300 - 44);
 
-  const listaIcones = nomesIcones.map((nomeIcone) => {
-    return icones[nomeIcone] || icones[NOME_ICONE_ERRO];
-  });
+  const listaIcones = nomesIcones.map((nomeIcone) => icones[nomeIcone] || icones[NOME_ICONE_ERRO]);
 
-  const largura = Math.min(iconesPorLinha * 300, nomesIcones.length * 300) - 44;
-  const altura = Math.ceil(listaIcones.length / iconesPorLinha) * 300 - 44;
+  const largura = Math.min(iconsPerLine * 300, nomesIcones.length * 300) - 44;
+  const altura = Math.ceil(listaIcones.length / iconsPerLine) * 300 - 44;
 
   const alturaRedimensionada = altura * ESCALA;
   const larguraRedimensionado = largura * ESCALA;
@@ -40,22 +15,19 @@ const gerarIcones = (nomesIcones, iconesPorLinha, icones, tamanho) => {
   return `
     <svg width="${larguraRedimensionado}" height="${alturaRedimensionada}" viewBox="0 0 ${largura} ${altura}" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
       ${listaIcones
-        .map(
-          (i, index) =>
-            `
-          <g transform="translate(${(index % iconesPorLinha) * 300}, ${
-              Math.floor(index / iconesPorLinha) * 300 // espaços
-            })">
+      .map(
+        (i, index) =>
+          `
+          <g transform="translate(${(index % iconsPerLine) * 300}, ${Math.floor(index / iconsPerLine) * 300 // espaços
+          })">
             ${i}
           </g>
           `
-        )
-        .join(" ")}
+      )
+      .join(" ")}
     </svg>
   `;
 };
 
-module.exports = {
-  carregarIcones,
-  gerarIcones,
-};
+module.exports = gerarIcones
+
