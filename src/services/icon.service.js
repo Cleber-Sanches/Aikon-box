f7c9c70b227446a34dba0fedd645300543abb6e0
Cleber Sanches <clebersanches59@gmail.com>
2024-03-04 21:35:51 -0300

    ♻️ refactor: Simplificação da função de geração de ícones

--- src/services/icon.service.js
+++ src/services/icon.service.js
@@ -1,33 +1,30 @@
+const ERROR_ICON_NAME = 'Error404';
+const SCALE = (tamanho) => tamanho / (300 - 44);
 
-const NOME_ICONE_ERRO = "Error404";
+const generateIcons = (iconNames, iconsPerLine, icons, size) => {
+  const scaledSize = SCALE(size);
 
-const gerarIcones = (nomesIcones, iconsPerLine, icones, tamanho) => {
-  const ESCALA = tamanho / (300 - 44);
+  const iconList = iconNames.map((name) => icons[name] || icons[ERROR_ICON_NAME]);
 
-  const listaIcones = nomesIcones.map((nomeIcone) => icones[nomeIcone] || icones[NOME_ICONE_ERRO]);
+  const width = Math.min(iconsPerLine * 300, iconNames.length * 300) - 44;
+  const height = Math.ceil(iconList.length / iconsPerLine) * 300 - 44;
 
-  const largura = Math.min(iconsPerLine * 300, nomesIcones.length * 300) - 44;
-  const altura = Math.ceil(listaIcones.length / iconsPerLine) * 300 - 44;
-
-  const alturaRedimensionada = altura * ESCALA;
-  const larguraRedimensionado = largura * ESCALA;
+  const scaledHeight = height * scaledSize;
+  const scaledWidth = width * scaledSize;
 
   return `
-    <svg width="${larguraRedimensionado}" height="${alturaRedimensionada}" viewBox="0 0 ${largura} ${altura}" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
-      ${listaIcones
-      .map(
-        (i, index) =>
-          `
-          <g transform="translate(${(index % iconsPerLine) * 300}, ${Math.floor(index / iconsPerLine) * 300 // espaços
-          })">
-            ${i}
+    <svg width="${scaledWidth}" height="${scaledHeight}" viewBox="0 0 ${width} ${height}" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+      ${iconList
+    .map(
+      (icon, index) => `
+          <g transform="translate(${(index % iconsPerLine) * 300}, ${Math.floor(index / iconsPerLine) * 300})">
+            ${icon}
           </g>
-          `
-      )
-      .join(" ")}
+          `,
+    )
+    .join(' ')}
     </svg>
   `;
 };
 
-module.exports = gerarIcones
-
+module.exports = generateIcons;
