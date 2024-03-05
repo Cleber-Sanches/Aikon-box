4be4ff0f7e894865a0a3e99fe56271db46fb161e
Cleber Sanches <clebersanches59@gmail.com>
2024-03-04 21:29:37 -0300

    🚚 chore: Ícones para pasta de assets

--- src/middlewares/errorHandler.js
+++
@@ -1,6 +0,0 @@
-function manipuladorErros(erro, req, res, next) {
-  console.error(`Erro na solicitação: ${erro.message}`);
-  res.status(500).send("Erro interno do servidor.");
-}
-
-module.exports = manipuladorErros;
