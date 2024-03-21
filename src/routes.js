e456a08ba903ba99da4ddba8c42a00417a2e0298
Cleber Sanches <clebersanches59@gmail.com>
2024-03-21 00:34:58 -0300

    ✨ feat: Add função para criação de SVGs personalizados

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const getIcons = require('./controllers/iconController');
 const getStatusCode = require('./controllers/statusCodeController');
-const getMethodsHttp = require('./controllers/methodsHttpController');
+const getMethodsHttp = require('./controllers/customSvgController');
+const getSvgWithModifiedText = require('./controllers/customSvgController');
 
 const routes = express();
 
 routes.get('/icons', getIcons);
 routes.get('/doc/statuscode', getStatusCode);
 routes.get('/doc/methods', getMethodsHttp);
+routes.get('/doc/custom', getSvgWithModifiedText);
 
 module.exports = routes;
